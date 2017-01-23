2a9a200319e226afb701dd7eb201675772f22b83
Jonester <chris.jones78@gmail.com>
2017-01-22 20:14:59 -0500

    Initial Commit

---
+++ BillSplitter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BillSplitter
+//
+//  Created by Chris Jones on 2017-01-22.
+//  Copyright © 2017 Jonescr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
